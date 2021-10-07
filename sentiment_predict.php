2b0854bc736fb8b9ff8d6565d7ff6bcaa248b5d6
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2021-10-08 00:45:11 +0700

    sentiment analysist

---
+++ sentiment_predict.php
@@ -0,0 +1,23 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Rubix\ML\PersistentModel;
+use Rubix\ML\Persisters\Filesystem;
+use Rubix\ML\Datasets\Unlabeled;
+
+ini_set('memory_limit', '-1');
+
+$estimator = PersistentModel::load(
+    new Filesystem('assets/rubix/sentiment.rbx')
+);
+
+while (empty($text)) $text = readline("Enter some text to analyze:\n");
+
+$dataset = new Unlabeled([
+    [$text],
+]);
+
+$prediction = current($estimator->predict($dataset));
+
+echo "The sentiment is: $prediction" . PHP_EOL;
